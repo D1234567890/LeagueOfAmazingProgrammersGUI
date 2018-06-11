254b73937da5607b94bfac7fb00f17f1c25f1b7d
D1234567890 <league@WTS-IM-6.attlocal.net>
2018-06-10 18:59:20 -0700

    Optimization
    Optimization Optimization Optimization

--- src/BoardPrototypeFinal.java
+++ src/BoardPrototypeFinal.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Optional;
 
+import com.sun.glass.ui.SystemClipboard;
+
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -22,6 +24,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class BoardPrototypeFinal extends Application {
@@ -31,6 +34,7 @@ public class BoardPrototypeFinal extends Application {
 
 	private boolean loadedData = false;
 	private static boolean timerOn = false;
+	private boolean timerActivatedBefore = false;
 
 	private static short width = 2000;
 	private static short height = 1000;
@@ -45,18 +49,16 @@ public class BoardPrototypeFinal extends Application {
 
 	private static Stage primaryStage;
 	private static Scene scene;
-	private static FlowPane flowpane;
-	private static Label levelLabel;
-	private static Button home, add, remove, change, promote;
-
-	private static Button levelZero, levelOne, levelTwo, levelThree, levelFour, levelFive, levelSix, levelSeven,
-			levelEight, levelNine;
+	private static FlowPane flowpane0, flowpane1, flowpane2, flowpane3, flowpane4, flowpane5, flowpane6, flowpane7,
+			flowpane8, flowpane9;
+	private static Label labelZero, labelOne, labelTwo, labelThree, labelFour, labelFive, labelSix, labelSeven,
+			labelEight, labelNine;
 
 	private static Font titleFont = new Font("Impact", 40);
 
 	private static ArrayList<Level> levels = new ArrayList<Level>();
 	private static ArrayList<Node> unremovableNodes = new ArrayList<Node>();
-	private static ArrayList<ImageView> images = new ArrayList<ImageView>();
+	private static ArrayList<Image> logos = new ArrayList<Image>();
 
 	public static void main(String[] args) {
 		new BoardPrototypeFinal();
@@ -65,46 +67,118 @@ public class BoardPrototypeFinal extends Application {
 
 	public void start(Stage alternateStage) throws Exception {
 
+		logos.add(new Image("HooverHS.jpg"));
+		logos.add(new Image("GompPrep.png"));
+		logos.add(new Image("LEAGUE.png"));
+		logos.add(new Image("MalcomX.png"));
+		logos.add(new Image("SanElijoMS.png"));
+		logos.add(new Image("SanMarcosMS.png"));
+		logos.add(new Image("SDCentral.jpg"));
+		logos.add(new Image("WilsonMS.jpg"));
+
 		primaryStage = new Stage();
 
-		flowpane = new FlowPane();
+		labelZero = new Label("Level 0");
+		labelOne = new Label("Level 1");
+		labelTwo = new Label("Level 2");
+		labelThree = new Label("Level 3");
+		labelFour = new Label("Level 4");
+		labelFive = new Label("Level 5");
+		labelSix = new Label("Level 6");
+		labelSeven = new Label("Level 7");
+		labelEight = new Label("Level 8");
+		labelNine = new Label("Level 9");
 
-		scene = new Scene(flowpane, width, height);
+		labelZero.setTranslateX(850);
+		labelOne.setTranslateX(850);
+		labelTwo.setTranslateX(850);
+		labelThree.setTranslateX(850);
+		labelFour.setTranslateX(850);
+		labelFive.setTranslateX(850);
+		labelSix.setTranslateX(850);
+		labelSeven.setTranslateX(850);
+		labelEight.setTranslateX(850);
+		labelNine.setTranslateX(850);
+		
+		labelZero.setFont(titleFont);
+		labelOne.setFont(titleFont);
+		labelTwo.setFont(titleFont);
+		labelThree.setFont(titleFont);
+		labelFour.setFont(titleFont);
+		labelFive.setFont(titleFont);
+		labelSix.setFont(titleFont);
+		labelSeven.setFont(titleFont);
+		labelEight.setFont(titleFont);
+		labelNine.setFont(titleFont);
+
+		flowpane0 = new FlowPane();
+		flowpane1 = new FlowPane();
+		flowpane2 = new FlowPane();
+		flowpane3 = new FlowPane();
+		flowpane4 = new FlowPane();
+		flowpane5 = new FlowPane();
+		flowpane6 = new FlowPane();
+		flowpane7 = new FlowPane();
+		flowpane8 = new FlowPane();
+		flowpane9 = new FlowPane();
+
+		flowpane0.setPadding(new Insets(20));
+		flowpane0.setVgap(VGap);
+		flowpane0.setHgap(HGap);
+		flowpane1.setPadding(new Insets(20));
+		flowpane1.setVgap(VGap);
+		flowpane1.setHgap(HGap);
+		flowpane2.setPadding(new Insets(20));
+		flowpane2.setVgap(VGap);
+		flowpane2.setHgap(HGap);
+		flowpane3.setPadding(new Insets(20));
+		flowpane3.setVgap(VGap);
+		flowpane3.setHgap(HGap);
+		flowpane4.setPadding(new Insets(20));
+		flowpane4.setVgap(VGap);
+		flowpane4.setHgap(HGap);
+		flowpane5.setPadding(new Insets(20));
+		flowpane5.setVgap(VGap);
+		flowpane5.setHgap(HGap);
+		flowpane6.setPadding(new Insets(20));
+		flowpane6.setVgap(VGap);
+		flowpane6.setHgap(HGap);
+		flowpane7.setPadding(new Insets(20));
+		flowpane7.setVgap(VGap);
+		flowpane7.setHgap(HGap);
+		flowpane8.setPadding(new Insets(20));
+		flowpane8.setVgap(VGap);
+		flowpane8.setHgap(HGap);
+		flowpane9.setPadding(new Insets(20));
+		flowpane9.setVgap(VGap);
+		flowpane9.setHgap(HGap);
+
+		flowpane0.getChildren().add(labelZero);
+		flowpane1.getChildren().add(labelOne);
+		flowpane2.getChildren().add(labelTwo);
+		flowpane3.getChildren().add(labelThree);
+		flowpane4.getChildren().add(labelFour);
+		flowpane5.getChildren().add(labelFive);
+		flowpane6.getChildren().add(labelSix);
+		flowpane7.getChildren().add(labelSeven);
+		flowpane8.getChildren().add(labelEight);
+		flowpane9.getChildren().add(labelNine);
+		
+		flowpane0.setStyle("-fx-background: gray;");
+		flowpane1.setStyle("-fx-background: tan;");
+		flowpane2.setStyle("-fx-background: red;");
+		flowpane3.setStyle("-fx-background: orange;");
+		flowpane4.setStyle("-fx-background: yellow;");
+		flowpane5.setStyle("-fx-background: green;");
+		flowpane6.setStyle("-fx-background: blue;");
+		flowpane7.setStyle("-fx-background: purple;");
+		flowpane8.setStyle("-fx-background: gray;");
+		flowpane9.setStyle("-fx-background: white;");
+
+		scene = new Scene(flowpane0, width, height);
 		scene.getStylesheets().add("test.css");
 
-		home = new Button("Home");
-
-		add = new Button("Add Name");
-		remove = new Button("Remove Name");
-		change = new Button("Change name");
-		promote = new Button("Promote Name");
-
-		levelZero = new Button("0");
-		levelOne = new Button("1");
-		levelTwo = new Button("2");
-		levelThree = new Button("3");
-		levelFour = new Button("4");
-		levelFive = new Button("5");
-		levelSix = new Button("6");
-		levelSeven = new Button("7");
-		levelEight = new Button("8");
-		levelNine = new Button("9");
-
-		levelLabel = new Label("The League");
-		levelLabel.setFont(titleFont);
-		unremovableNodes.add(levelLabel);
-
-		flowpane.getChildren().addAll(levelLabel);
-
-		levelLabel.setTranslateX(850);
-
-		flowpane.setPadding(new Insets(20));
-		flowpane.setVgap(VGap);
-		flowpane.setHgap(HGap);
-		flowpane.setStyle("-fx-background: orange;");
-
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("League of Amazing Programmers GUI");
 		primaryStage.show();
 
 		levels.add(new Level((byte) 0));
@@ -118,107 +192,11 @@ public class BoardPrototypeFinal extends Application {
 		levels.add(new Level((byte) 8));
 		levels.add(new Level((byte) 9));
 
-		add.setOnAction(new EventHandler<ActionEvent>() {
+		if (!timerActivatedBefore) {
+			BoardPrototypeFinal.timerController();
+		}
 
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.addName();
-			}
-
-		});
-
-		remove.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.removeName();
-			}
-
-		});
-
-		change.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.changeName();
-			}
-
-		});
-
-		promote.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.promoteName();
-			}
-
-		});
-
-		levelZero.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.displayLevel((byte) 0);
-			}
-		});
-
-		levelOne.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.displayLevel((byte) 1);
-			}
-		});
-
-		levelTwo.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.displayLevel((byte) 2);
-			}
-		});
-
-		levelThree.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.displayLevel((byte) 3);
-			}
-		});
-
-		levelFour.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.displayLevel((byte) 4);
-			}
-		});
-
-		levelFive.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.displayLevel((byte) 5);
-			}
-		});
-
-		levelSix.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.displayLevel((byte) 6);
-
-			}
-		});
-
-		levelSeven.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.displayLevel((byte) 7);
-			}
-		});
-
-		levelEight.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.displayLevel((byte) 8);
-			}
-		});
-
-		levelNine.setOnAction(new EventHandler<ActionEvent>() {
-
-			public void handle(ActionEvent event) {
-				BoardPrototypeFinal.displayLevel((byte) 9);
-			}
-		});
+		timerOn = true;
 
 		scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
 
@@ -265,9 +243,10 @@ public class BoardPrototypeFinal extends Application {
 				ctrlPressed = false;
 				BoardPrototypeFinal.promoteName();
 			} else if (key.getCode() == KeyCode.N && ctrlPressed) {
+				if (!timerActivatedBefore)
+					BoardPrototypeFinal.timerController();
 				ctrlPressed = false;
 				timerOn = true;
-				BoardPrototypeFinal.timerController();
 			} else if (key.getCode() == KeyCode.F && ctrlPressed) {
 				ctrlPressed = false;
 				timerOn = false;
@@ -314,22 +293,101 @@ public class BoardPrototypeFinal extends Application {
 
 			loadedData = true;
 		}
+
+		for (byte i = 0; i < levels.size(); i++) {
+
+			for (Student student : levels.get(i).getStudents()) {
+
+				TextFlow studentLabel = new TextFlow();
+				Text name = new Text(student.getName());
+				name.setId("fancytext");
+				ImageView imageView = new ImageView("LEAGUE.png");
+				studentLabel.getChildren().add(name);
+				final String loc = student.getLocation();
+
+				switch (loc) {
+				case "HH":
+					imageView.setImage(logos.get(0));
+					break;
+				case "GP":
+					imageView.setImage(logos.get(1));
+					break;
+				case "CV":
+					imageView.setImage(logos.get(2));
+					break;
+				case "MX":
+					imageView.setImage(logos.get(3));
+					break;
+				case "SE":
+					imageView.setImage(logos.get(4));
+					break;
+				case "SM":
+					imageView.setImage(logos.get(5));
+					break;
+				case "DL":
+					imageView.setImage(logos.get(6));
+					break;
+				case "WM":
+					imageView.setImage(logos.get(7));
+					break;
+				}
+
+				imageView.setPreserveRatio(true);
+				imageView.setFitHeight(30);
+				studentLabel.getChildren().add(imageView);
+				studentLabel.setTranslateY(100);
+
+				switch (i) {
+				case 0:
+					flowpane0.getChildren().add(studentLabel);
+					break;
+				case 1:
+					flowpane1.getChildren().add(studentLabel);
+					break;
+				case 2:
+					flowpane2.getChildren().add(studentLabel);
+					break;
+				case 3:
+					flowpane3.getChildren().add(studentLabel);
+					break;
+				case 4:
+					flowpane4.getChildren().add(studentLabel);
+					break;
+				case 5:
+					flowpane5.getChildren().add(studentLabel);
+					break;
+				case 6:
+					flowpane6.getChildren().add(studentLabel);
+					break;
+				case 7:
+					flowpane7.getChildren().add(studentLabel);
+					break;
+				case 8:
+					flowpane8.getChildren().add(studentLabel);
+					break;
+				case 9:
+					flowpane9.getChildren().add(studentLabel);
+					break;
+				}
+			}
+		}
 	}
 
 	public static void timerController() {
 		new AnimationTimer() {
-			long lastUpdate = 0;
+			long millis = System.currentTimeMillis();
 
 			public void handle(long now) {
-				if (now - lastUpdate >= 2_000_000_000l && timerOn) {
-					BoardPrototypeFinal.displayLevel(levelNum);
-					if (levelNum < 9) {
-						levelNum++;
-					} else {
-						levelNum = 0;
+				if (timerOn) {
+					if (System.currentTimeMillis() - millis >= 10000) {
+						millis = System.currentTimeMillis();
+						BoardPrototypeFinal.displayLevel(levelNum);
+						if (levelNum < 9) {
+							levelNum++;
+						} else {
+							levelNum = 0;
+						}
 					}
-
-					lastUpdate = now;
 				}
 			}
 		}.start();
@@ -377,102 +435,35 @@ public class BoardPrototypeFinal extends Application {
 
 		switch (levelNum) {
 		case 0:
-			flowpane.setStyle("-fx-background: gray;");
-			levelLabel.setText("Level 0");
+			scene.setRoot(flowpane0);
 			break;
 		case 1:
-			flowpane.setStyle("-fx-background: tan;");
-			levelLabel.setText("Level 1");
+			scene.setRoot(flowpane1);
 			break;
 		case 2:
-			flowpane.setStyle("-fx-background: red;");
-			levelLabel.setText("Level 2");
+			scene.setRoot(flowpane2);
 			break;
 		case 3:
-			flowpane.setStyle("-fx-background: orange;");
-			levelLabel.setText("Level 3");
+			scene.setRoot(flowpane3);
 			break;
 		case 4:
-			flowpane.setStyle("-fx-background: yellow;");
-			levelLabel.setText("Level 4");
+			scene.setRoot(flowpane4);
 			break;
 		case 5:
-			flowpane.setStyle("-fx-background: green;");
-			levelLabel.setText("Level 5");
+			scene.setRoot(flowpane5);
 			break;
 		case 6:
-			flowpane.setStyle("-fx-background: blue;");
-			levelLabel.setText("Level 6");
+			scene.setRoot(flowpane6);
 			break;
 		case 7:
-			flowpane.setStyle("-fx-background: purple;");
-			levelLabel.setText("Level 7");
+			scene.setRoot(flowpane7);
 			break;
 		case 8:
-			flowpane.setStyle("-fx-background: gray;");
-			levelLabel.setText("Level 8");
+			scene.setRoot(flowpane8);
 			break;
 		case 9:
-			flowpane.setStyle("-fx-background: white;");
-			levelLabel.setText("Level 9");
+			scene.setRoot(flowpane9);
 			break;
-		}
-
-		for (int i = 0; i < flowpane.getChildren().size(); i++) {
-			if (flowpane.getChildren().get(i) instanceof TextFlow
-					&& !(unremovableNodes.contains(flowpane.getChildren().get(i)))) {
-				flowpane.getChildren().remove(i);
-				i--;
-			}
-		}
-
-		for (Level level : levels) {
-
-			if (level.getLevel() == levelNum) {
-
-				for (Student student : level.getStudents()) {
-
-					TextFlow studentLabel = new TextFlow();
-					Text name = new Text(student.getName());
-					name.setId("fancytext");
-					ImageView imageView = new ImageView("LEAGUE.png");
-					studentLabel.getChildren().add(name);
-					final String loc = student.getLocation();
-
-					switch (loc) {
-					case "HH":
-						imageView = new ImageView("HooverHS.jpg");
-						break;
-					case "GP":
-						imageView = new ImageView("GompPrep.png");
-						break;
-					case "CV":
-						imageView = new ImageView("LEAGUE.png");
-						break;
-					case "MX":
-						imageView = new ImageView("MalcomX.png");
-						break;
-					case "SE":
-						imageView = new ImageView("SanElijoMS.png");
-						break;
-					case "SM":
-						imageView = new ImageView("SanMarcosMS.png");
-						break;
-					case "DL":
-						imageView = new ImageView("SDCentral.jpg");
-						break;
-					case "WM":
-						imageView = new ImageView("WilsonMS.jpg");
-						break;
-					}
-
-					imageView.setPreserveRatio(true);
-					imageView.setFitHeight(30);
-					studentLabel.getChildren().add(imageView);
-					studentLabel.setTranslateY(100);
-					flowpane.getChildren().add(studentLabel);
-				}
-			}
 		}
 	}
 
